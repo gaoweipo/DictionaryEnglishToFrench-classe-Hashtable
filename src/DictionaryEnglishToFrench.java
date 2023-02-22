9a9080dd0159546f761028e4757b0f56a4c71940
gaoweipo <gaoweipo@hotmail.com>
2023-02-21 23:08:42 -0500

    Finish the programme.

--- src/DictionaryEnglishToFrench.java
+++ src/DictionaryEnglishToFrench.java
@@ -9,7 +9,16 @@ public class DictionaryEnglishToFrench {
 ht.put("city","ville");
 ht.put("country","pays");
         System.out.println(ht);
-
+if (ht.isEmpty())
+    System.out.println("There is no word in the dictionary.");
+else{
+    System.out.println("Enter a word here:");
+    String Key=sc.nextLine();
+if (ht.containsKey(Key.toLowerCase()))
+    System.out.println("The translation of the word "+Key.toLowerCase()+" est "+ht.get(Key.toLowerCase()));
+else
+    System.out.println("This word is not in the dictionary.");
+}
     }
 
 }
